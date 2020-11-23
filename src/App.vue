d13b3ddfd0d851b714cb7ddf05b2491d1c6873d8
caogzh <243509301@qq.com>
2020-11-23 23:06:41 +0800

    学习响应式对象，生命周期，watch监测

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <h1>{{count}}</h1>
-    <h1>{{double}}</h1>
+    <h1>{{ count }}</h1>
+    <h1>{{ double }}</h1>
+    <ul>
+      <li v-for="number in numbers" :key="number">
+        <h1>{{ number }}</h1>
+      </li>
+    </ul>
+    <h1>{{ persion.name }}</h1>
+    <h1>{{ greetings }}</h1>
     <button @click="increase">+1</button>
+    <button @click="updateGreeting">updateTitle</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { ref, computed, reactive, toRefs}  from 'vue'; 
+import {
+  ref,
+  computed,
+  reactive,
+  toRefs,
+  onMounted,
+  onUpdated,
+  onRenderTriggered,
+  watch,
+} from "vue";
 interface DataProps {
   count: number;
   double: number;
   increase: () => void;
+  numbers: number[];
+  persion: { name?: string };
 }
 export default defineComponent({
   name: "App",
@@ -25,16 +44,42 @@ export default defineComponent({
     // const double = computed(()=>{
     //   return count.value * 2
     // })
+    onMounted(() => {
+      console.log("on mounted");
+    });
+    onUpdated(() => {
+      console.log("on updated");
+    });
+    onRenderTriggered((event) => {
+      console.log(event);
+    });
     const data: DataProps = reactive({
       count: 0,
       double: computed(() => data.count * 2),
-      increase: () => {data.count ++}
-    })
-    const dataRefs = toRefs(data)
+      increase: () => {
+        data.count++;
+      },
+      numbers: [1, 2, 3],
+      persion: {},
+    });
+    data.numbers[0] = 5;
+    data.persion.name = "lage";
+    const greetings = ref("");
+    const updateGreeting = () => {
+      greetings.value += "hello!";
+    };
+    watch([greetings, () => data.count], (newValue, oldValue) => {
+      console.log(newValue);
+      console.log(oldValue);
+      document.title = "update" + greetings.value + data.count;
+    });
+    const dataRefs = toRefs(data);
     return {
-      ...dataRefs
-    }
-  }
+      ...dataRefs,
+      greetings,
+      updateGreeting,
+    };
+  },
 });
 </script>
 
