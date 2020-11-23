807542873f9b709d5693c10d41c61cee858adce1
caogzh <243509301@qq.com>
2020-11-23 20:13:15 +0800

    练习ref，computed, reactive, toRefs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>{{count}}</h1>
+    <h1>{{double}}</h1>
+    <button @click="increase">+1</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent } from "vue";
+import { ref, computed, reactive, toRefs}  from 'vue'; 
+interface DataProps {
+  count: number;
+  double: number;
+  increase: () => void;
+}
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  setup() {
+    // const count = ref(0)
+    // const increment = () => {
+    //   count.value ++
+    // }
+    // const double = computed(()=>{
+    //   return count.value * 2
+    // })
+    const data: DataProps = reactive({
+      count: 0,
+      double: computed(() => data.count * 2),
+      increase: () => {data.count ++}
+    })
+    const dataRefs = toRefs(data)
+    return {
+      ...dataRefs
+    }
   }
 });
 </script>
